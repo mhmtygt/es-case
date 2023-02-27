85863f9129bd356f1af49f6f085aa00a99ac887a
Mahmut Yiğit <mahmutyigit@Mahmuts-MBP-Max.home>
2023-02-28 01:02:45 +0300

    added persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import formReducer from "./slices/formSlice";
-
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, formReducer);
+
 export const store = configureStore({
-  reducer: { formState: formReducer },
+  reducer: { formState: persistedReducer },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
+
+export const persistor = persistStore(store);
